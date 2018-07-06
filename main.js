942dba295c325bca5d3c23d5512fb9fc638cedf9
aaryan10 <aaryanbudhiraja2@gmail.com>
2018-07-06 20:07:36 +0530

    origin

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ let minutes = now.getMinutes();
 let seconds = now.getSeconds();
 
 
-hours = (hours % 12) * 30;
-seconds = (seconds/60) * 360;
-minutes = (minutes/60) * 360;
+hours = ((hours % 12) * 30) - 90;
+seconds = ((seconds/60) * 360) - 90;
+minutes = ((minutes/60) * 360) - 90;
 document.getElementsByClassName('seconds')[0].style.transform = "rotateZ("+seconds+"deg)";
 document.getElementsByClassName('minutes')[0].style.transform = "rotateZ("+minutes+"deg)";
 document.getElementsByClassName('hours')[0].style.transform = "rotateZ("+hours+"deg)";
