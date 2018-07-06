1515bb50c23f62f144c38bb7039488d2182fa1e1
aaryan10 <aaryanbudhiraja2@gmail.com>
2018-07-06 17:08:37 +0530

    origin

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ let hours = now.getHours();
 let minutes = now.getMinutes();
 let seconds = now.getSeconds();
 
-if(hours<12) hours = (hours/12) *360;
-else if(hours>12) hours = (hours-12/12) *360;
 
+hours = (hours % 12) * 30;
 seconds = (seconds/60) * 360;
 minutes = (minutes/60) * 360;
-document.getElementsByClassName('seconds').style.transform = "rotateZ("+seconds+"deg)";
-document.getElementsByClassName('minutes').style.transform = "rotateZ("+minutes+"deg)";
-document.getElementsByClassName('hours').style.transform = "rotateZ("+hours+"deg)";
+document.getElementsByClassName('seconds')[0].style.transform = "rotateZ("+seconds+"deg)";
+document.getElementsByClassName('minutes')[0].style.transform = "rotateZ("+minutes+"deg)";
+document.getElementsByClassName('hours')[0].style.transform = "rotateZ("+hours+"deg)";
 
 }
 
